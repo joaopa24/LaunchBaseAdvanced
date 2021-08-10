c6d65cb40b5eb6a736069d1d2b6bb5f727eb1003
joaopa24 <joaokn129@gmail.com>
2021-08-10 16:42:59 -0300

    base models

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -1,6 +1,19 @@
 const db = require('../../config/db')
-const { create } = require('./Product')
 
+function find(filters, table) {
+    let query = `SELECT * FROM ${table}`
+
+    Object.keys(filters).map(key => {
+        // WHERE | OR | AND
+        query += `${key}`
+
+        Object.keys(filters[key]).map(field => {
+            query += `${field} = '${filters[key][field]}'`
+        })
+    })
+
+    return db.query(query)
+}
 const Base = {
     init({ table }) {
         if (!table) throw new Error('Invalid Params')
@@ -9,27 +22,25 @@ const Base = {
 
         return this
     },
-    async findOne(filters) {
-        let query = `SELECT * FROM ${this.table}`
+    async find(id) {
+        const results = await find({where: {id} }, this.table)
 
-        Object.keys(filters).map(key => {
-            // WHERE | OR | AND
-            query = `${query}
-            ${key}
-            `
-
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            })
-        })
-
-        const results = await db.query(query)
         return results.rows[0]
     },
-    async create(fields){ // User.create({ name: 'Mayk'})
-        try{
+    async findOne(filters) {
+        const results = await find(filters, this.table)
+
+        return results.rows[0]
+    },
+    async findAll(filters) {
+        const results = await find(filters, this.table)
+
+        return results.rows
+    },
+    async create(fields) { // User.create({ name: 'Mayk'})
+        try {
             let keys = [],
-            values = []
+                values = []
 
             Object.keys(fields).map(key => {
                 keys.push(key)
@@ -43,12 +54,12 @@ const Base = {
             const results = await db.query(query)
             return results.rows[0].id
 
-        } catch(error){
+        } catch (error) {
             console.error(error)
         }
     },
     update(id, fields) {
-        try{
+        try {
             let update = []
 
             Object.keys(fields).map(key => {
@@ -56,17 +67,17 @@ const Base = {
                 const line = `${key} = '${fields[key]}'`
                 update.push(line)
             })
-    
+
             let query = `UPDATE ${this.table} SET ${update.join(',')} WHERE id = ${id}`
-    
+
             return db.query(query)
-                
-        }catch(error){
+
+        } catch (error) {
             console.error(error)
         }
     },
-    delete(id){
-        return db.query('DELETE FROM products WHERE id = $1', [id])
+    delete(id) {
+        return db.query(`DELETE FROM ${this.table} WHERE id = $1`, [id])
     }
 }
 
